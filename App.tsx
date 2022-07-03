ef7f062295691b258920889ac6e0492623c06f0a
Abdenour Touat <61945965+abdenour-progdes@users.noreply.github.com>
2022-07-03 11:31:50 +0100

    initialize the project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { RootStackParamList } from "./typing";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/view/pages/HomeScreen";
+import DetailsScreen from "./src/view/pages/DetailsScreen";
+import COLORS from "./src/consts/colors";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor={COLORS.green} />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/view/pages/DetailsScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
