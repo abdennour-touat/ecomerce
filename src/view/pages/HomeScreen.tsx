d285cccce4cd473483a72a7eba0dd39374c04764
Abdenour Touat <61945965+abdenour-progdes@users.noreply.github.com>
2022-07-04 11:02:31 +0100

    creating the header

--- src/view/pages/HomeScreen.tsx
+++ src/view/pages/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Header,
+  Heading,
+  SafeAreaContainer,
+} from "../../styles/HomScreen.style";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <SafeAreaContainer>
+      <Header>
+        <View>
+          <Heading subTitle>Welcome to </Heading>
+          <Heading title>Plant shop</Heading>
+        </View>
+      </Header>
+    </SafeAreaContainer>
   );
 };
 
